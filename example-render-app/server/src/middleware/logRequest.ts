b98b53ba3d1e380c0e071a5d0b5415eab40669d2
Jeremy Osner <anacreon@gmail.com>
2025-01-10 13:45:05 -0500

    remove

--- example-render-app/server/src/middleware/logRequest.ts
+++
@@ -1,6 +0,0 @@
-const logRequest = (req: any, _res: any, next: any) => {
-  console.log(`${req.method} request received to ${req.url}`);
-  next();
-};
-
-export default logRequest;
--- example-render-app/server/src/routes/api/index.ts
+++
@@ -1,9 +0,0 @@
-import tipRoutes from './tipsRoutes.js';
-import feedbackRoutes from './feedbackRoutes.js';
-import express from 'express';
-const router = express.Router();
-
-router.use('/tips', tipRoutes);
-router.use('/feedback', feedbackRoutes);
-
-export default router;
